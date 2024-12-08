d83da4df1eec00c01b9a7c0f85c993696ed6f14f
Rahul Raj <rahul.raj9237@gmail.com>
2024-12-08 18:11:58 +0530

    logged with morgan and winston

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-
 import 'dotenv/config'
 import express from 'express'
 
+import logger from "./logger.js";
+import morgan from "morgan";
 const app=express()
+const morganFormat = ":method :url :status :response-time ms";
+const port = process.env.PORT || 3000
 
-const port =process.env.PORT || 3000
-
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
 
 //accept the data from frontedn
 
